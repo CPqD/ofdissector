f8d23e07d45f971156dd3c1fc1e4ba6c81e829ab
Allan Vidal <allanv@cpqd.com.br>
2012-07-04 15:15:32 -0300

    Included code generator.

---
+++ gen/ofgen.py
@@ -0,0 +1,89 @@
+# This is a very nasty code generator. It will get better someday.
+
+IDENT = " " * 4
+OFSPEC = "ofspec"
+
+class Enum:
+    def __init__(self, name, bitmap=False, length=32):
+        self.name = name
+        self.bitmap = bitmap
+        self.lines = []
+
+        # Calc reserved bits
+        self.length = length
+        self.mask = [0 for x in range(length)]
+
+    def add(self, field, value, desc):
+        if current.bitmap:
+            try:
+                bit = int(value.split("<<")[1].strip())
+                current.add_masked_bit(bit)
+            except IndexError: # Ignore unshifted things
+                return
+            self.lines.append("this->mFM.createField(\"{0}.{1}\", \"{2}\", FT_BOOLEAN, 32, TFS(&tfs_set_notset), {1});".format(self.name, field, desc));
+        else:
+            self.lines.append("TYPE_ARRAY_ADD({0}, {1}, \"{2} - {3}\");".format(self.name, field, desc, field))
+
+    def add_masked_bit(self, bit):
+        self.mask[bit] = 1
+
+    def get_reserved(self):
+        self.mask.reverse()
+        result = hex(int("0b" + "".join([str(int(not d)) for d in self.mask]), 2)).strip("L")
+        self.mask.reverse()
+        return result
+
+    def __str__(self):
+        s = "// %s\n" % self.name
+        if not self.bitmap:
+            s += "TYPE_ARRAY(%s);\n" % self.name
+        s += "\n".join(self.lines)
+        if self.bitmap:
+            s += "\nthis->mFM.createField(\"{0}.RESERVED\", \"Reserved\", FT_BOOLEAN, 32, TFS(&tfs_set_notset), {1});".format(self.name, self.get_reserved())
+        return s
+
+f = open(OFSPEC, "r")
+lines = [i.strip("\n").split(" / ") for i in f.readlines()]
+enums = []
+
+for row in lines:
+    if row == [""]:
+        continue
+    if len(row) == 1:
+        name = row[0]
+        bitmap = False
+        if (name[-1] == "*"):
+            name = name[:-1]
+            bitmap = True
+        enum = Enum(name, bitmap)
+        enums.append(enum)
+        continue
+
+    current = enums[-1]
+    field, value, desc = row
+    desc = desc.replace("\"", "\\\"")
+    current.add(field, value, desc)
+
+for enum in enums:
+    if not enum.bitmap:
+        print "GArray* %s;" % enum.name
+
+print
+print
+
+print "void DissectorContext::setupCodes(void) {"
+for enum in enums:
+    if not enum.bitmap:
+        print IDENT + str(enum).replace("\n", "\n" + IDENT)
+        print
+print "}"
+
+print
+print
+
+print "void DissectorContext::setupFlags(void) {"
+for enum in enums:
+    if enum.bitmap:
+        print IDENT + str(enum).replace("\n", "\n" + IDENT)
+        print
+print "}"
